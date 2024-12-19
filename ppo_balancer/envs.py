56f4fa9f867971cc742137a52eda682a6a7a3791
Stéphane Caron <stephane.caron@inria.fr>
2024-12-19 10:01:25 +0100

    Refactor environment wrappers

--- ppo_balancer/envs.py
+++ ppo_balancer/envs.py
@@ -4,9 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright 2023 Inria
 
-import gymnasium
+import gymnasium as gym
 import numpy as np
-from gymnasium import spaces
 from gymnasium.wrappers import FrameStack, RescaleAction
 from settings import EnvSettings
 from upkie.envs import UpkieGroundVelocity
@@ -18,52 +17,37 @@ from upkie.envs.wrappers import (
     NoisifyObservation,
 )
 
+from ppo_balancer.wrappers import WheeledInvertedPendulumReward
 
-def make_training_env(
-    velocity_env: UpkieGroundVelocity,
+
+def add_training_wrappers(
+    original_env: gym.Env,
     env_settings: EnvSettings,
-) -> gymnasium.Wrapper:
+) -> gym.Wrapper:
     action_noise = np.array(env_settings.action_noise)
     observation_noise = np.array(env_settings.observation_noise)
-    noisy_obs_env = NoisifyObservation(velocity_env, noise=observation_noise)
+    noisy_obs_env = NoisifyObservation(original_env, noise=observation_noise)
     noisy_env = NoisifyAction(noisy_obs_env, noise=action_noise)
     filtered_env = AddLagToAction(
         noisy_env,
-        time_constant=spaces.Box(*env_settings.action_lpf),
+        time_constant=gym.spaces.Box(*env_settings.action_lpf),
     )
     return filtered_env
 
 
-def make_accel_env(
-    velocity_env: UpkieGroundVelocity,
+def make_ppo_balancer_env(
+    env: UpkieGroundVelocity,
     env_settings: EnvSettings,
     training: bool,
-) -> gymnasium.Wrapper:
-    inner_env = (
-        make_training_env(velocity_env, env_settings)
-        if training
-        else velocity_env
-    )
-    history_env = FrameStack(
-        AddActionToObservation(inner_env),
-        env_settings.history_size,
-    )
-    accel_env = DifferentiateAction(
-        history_env,
+) -> gym.Wrapper:
+    env = WheeledInvertedPendulumReward(env)
+    if training:
+        env = add_training_wrappers(env, env_settings)
+    env = FrameStack(AddActionToObservation(env), env_settings.history_size)
+    env = DifferentiateAction(
+        env,
         min_derivative=-env_settings.max_ground_accel,
         max_derivative=+env_settings.max_ground_accel,
     )
-    rescaled_accel_env = RescaleAction(
-        accel_env,
-        min_action=-1.0,
-        max_action=+1.0,
-    )
-    return rescaled_accel_env
-
-
-def make_ppo_balancer_env(
-    velocity_env: UpkieGroundVelocity,
-    env_settings: EnvSettings,
-    training: bool,
-) -> gymnasium.Wrapper:
-    return make_accel_env(velocity_env, env_settings, training=training)
+    env = RescaleAction(env, min_action=-1.0, max_action=1.0)
+    return env
